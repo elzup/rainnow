8e52f80855408d7861e3d97bbad83ba12b315dc4
elzup <guild0105@gmail.com>
2017-07-31 17:16:27 +0900

    Position style

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import _ from 'lodash'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
+    const num = 12
+    const deg = 360.0 / num
+    const r = 100
+    const red = deg * Math.PI / 180.0
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>CPS Lab 14F Rainnow Controller</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <section
+            class="main-contents"
+            style={{ display: 'flex', justifyContent: 'center', padding: 30 }}
+          >
+            <section class="circle-box" style={{ position: 'relative' }}>
+              {_.map(_.range(0, 12), i => {
+                const x = Math.cos(red * i) * r
+                const y = Math.sin(red * i) * r + r
+                return (
+                  <div
+                    key={i}
+                    style={{
+                      position: 'absolute',
+                      left: x,
+                      top: y,
+                    }}
+                  >
+                    {i}
+                  </div>
+                )
+              })}
+            </section>
+          </section>
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
