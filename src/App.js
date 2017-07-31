41a94b82878167de5c38f3c464a922484e3d0639
elzup <guild0105@gmail.com>
2017-07-31 17:56:58 +0900

    Color picker

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
+import { SketchPicker } from 'react-color'
 import _ from 'lodash'
+
 import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+  state = {
+    leds: _.times({ r: 0, g: 0, b: 0, off: true, blink: true }),
+    color: null,
+  }
+  static onClick() {}
   render() {
     const num = 12
     const deg = 360.0 / num
@@ -17,27 +24,40 @@ class App extends Component {
         <p className="App-intro">
           <section
             class="main-contents"
-            style={{ display: 'flex', justifyContent: 'center', padding: 30 }}
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              padding: 30,
+            }}
           >
-            <section class="circle-box" style={{ position: 'relative' }}>
-              {_.map(_.range(0, 12), i => {
-                const it = (i + 9) % 12
-                const x = -Math.cos(red * it) * r
-                const y = Math.sin(red * it) * r + r
-                return (
-                  <div
-                    key={i}
-                    style={{
-                      position: 'absolute',
-                      left: x,
-                      top: y,
-                    }}
-                  >
-                    {i}
-                  </div>
-                )
-              })}
-            </section>
+            <div>
+              <div
+                class="circle-box"
+                style={{ position: 'relative', height: 350 }}
+              >
+                {_.map(_.range(0, 12), i => {
+                  const it = (i + 9) % 12
+                  const x = -Math.cos(red * it) * r + r
+                  const y = Math.sin(red * it) * r + r
+                  return (
+                    <div
+                      key={i}
+                      style={{
+                        position: 'absolute',
+                        left: x,
+                        top: y,
+                      }}
+                    >
+                      {i}
+                    </div>
+                  )
+                })}
+              </div>
+              <SketchPicker
+                color={this.state.background}
+                onChangeComplete={this.handleChangeComplete}
+              />
+            </div>
           </section>
         </p>
       </div>
